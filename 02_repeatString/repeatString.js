ba4f04122005b1a7b5fbd9ff7701490fe1fd15e2
layden diaz <layden@student.dev-island.org>
2023-06-22 19:28:47 -1000

    thanks for the command, fixed hello wrld

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function (sentence, howManyItterations) {
+  for (i = 0; i < howManyItterations; i++) {
+    return sentence;
+  }
 };
 
 // Do not edit below this line
