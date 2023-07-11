0307f2b2b7412358d7d2d39e1afa0f30d65dfd08
layden diaz <layden@student.dev-island.org>
2023-07-11 12:06:16 -1000

    solved calculator tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function (numOne, numTwo) {
+  return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function (numOne, numTwo) {
+  return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function (numberArray) {
+  let result = 0;
+  for (let i = 0; i < numberArray.length; i++) {
+    result += numberArray[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (...numArray) {
+  let result = 1;
+  for (let i = 0; i < numArray.length; i++) {
+    result *= numArray[i];
+    // console.log(result);
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (multiple, power) {
+  let result = multiple;
+  for (let i = 1; i < power; i++) {
+    result *= multiple;
+    console.log(multiple);
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+
+  if (num < 1) {
+    return 1;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    for (let i = num; i > 0; i--) {
+      result *= i;
+    }
+
+    return result;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +55,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
